121da54f114b2d62476c6e3d1cbb9de683dfe367
Orion-Pro <jules.wygas@gmail.com>
2026-03-04 18:49:49 -0500

    implemented List interface, and LinkedList class

--- src/main/java/Model/linkedList/LinkedList.java
+++ src/main/java/Model/linkedList/LinkedList.java
@@ -1,4 +1,49 @@
 package Model.linkedList;
 
-public class LinkedList {
+public class LinkedList<T> implements List<T> {
+
+    private class Node<T> {
+        T data;
+        Node<T> prev;
+        Node<T> next;
+
+        public Node(T item) {
+            data = item;
+            next = null;
+        }
+    }
+
+    private Node<T> head = null;
+    private Node<T> tail = null;
+    private int n;
+
+    @Override
+    public void push(T item) {
+        Node<T> newNode = new Node<>(item);
+        tail.next = newNode;
+        newNode.prev = tail;
+        tail = newNode;
+        n++;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    @Override
+    public T pop() {
+        Node<T> removed = tail;
+        tail = null;
+        tail = removed.prev;
+        n--;
+        return removed.data;
+    }
+
+    @Override
+    public int size() {return n;}
+
+
+
+
 }
--- src/main/java/Model/linkedList/List.java
+++ src/main/java/Model/linkedList/List.java
@@ -1,4 +1,8 @@
 package Model.linkedList;
 
 public interface List<T> {
+    public void push(T item);
+    public T pop();
+    public boolean isEmpty();
+    public int size();
 }
